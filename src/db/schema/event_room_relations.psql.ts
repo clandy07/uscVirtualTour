100275209312def8b38a0af7cf15b8d99c512f0b
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-12 00:05:12 +0800

    finished schema definition of event_room_relations. only lacks relations definition

--- src/db/schema/event_room_relations.psql.ts
+++ src/db/schema/event_room_relations.psql.ts
@@ -1,7 +1,11 @@
-import { integer, pgTable } from "drizzle-orm/pg-core";
-import { timestamps } from './columns.helpers';
+import { integer, pgTable, primaryKey, index } from "drizzle-orm/pg-core";
+import { events } from "./events.psql";
+import { rooms } from "./rooms.psql";
 
 export const event_room_relations = pgTable("event_room_relations", {
-    event_id: integer(),
-    room_id: integer()
-});
+    event_id: integer().references(() => events.id),
+    room_id: integer().references(() => rooms.id)
+}, (table) => [
+    primaryKey({ name: 'event_room_composite_pk', columns: [table.event_id, table.room_id] }),
+    index("event_room_composite_idx").on(table.event_id, table.room_id)
+]);
