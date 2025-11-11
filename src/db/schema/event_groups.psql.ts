b99746bccf8bf7d82597664e53ba83714032ff26
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-11 22:37:41 +0800

    finished schema definition. only lacks relations definition.

--- src/db/schema/event_groups.psql.ts
+++ src/db/schema/event_groups.psql.ts
@@ -1,13 +1,15 @@
-import { integer, pgTable, varchar, text, timestamp } from "drizzle-orm/pg-core";
+import { integer, index, serial, pgTable, varchar, text, timestamp } from "drizzle-orm/pg-core";
 import { timestamps } from './columns.helpers';
 
 export const event_groups = pgTable("event_groups", {
-    id:integer().primaryKey(),
+    id:serial().primaryKey(),
     name: varchar({ length: 255 }).notNull(),
     description: text(),
-    location_id: integer(),
     date_time_start: timestamp(),
     date_time_end: timestamp(),
     custom_marker: text(),
     ...timestamps
-});
+}, (table) => [
+    index("date_time_start_idx").on(table.date_time_start),
+    index("date_time_end_idx").on(table.date_time_end)
+]);
