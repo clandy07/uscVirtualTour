1ac8913b18b2cfb92dc0f6228f1a0ca4fa57dcca
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-11 22:45:35 +0800

    finished this table's schema definition. its only missing its relations

--- src/db/schema/event_group_location_relations.psql.ts
+++ src/db/schema/event_group_location_relations.psql.ts
@@ -1,8 +1,11 @@
-import { integer, pgTable } from "drizzle-orm/pg-core";
-import { timestamps } from './columns.helpers';
+import { integer, pgTable, primaryKey, index } from "drizzle-orm/pg-core";
+import { event_groups } from "./event_groups.psql";
+import { locations } from "./locations.psql";
 
 export const event_group_location_relations = pgTable("event_group_location_relations", {
-  location_id: integer(),
-  event_group_id: integer(),
-  ...timestamps
-});
+  location_id: integer().references(() => locations.id),
+  event_group_id: integer().references(() => event_groups.id)
+}, (table) => [
+    primaryKey({ name: 'location_event_group_composite_pk', columns: [table.location_id, table.event_group_id] }),
+    index('location_event_group_composite_idx').on(table.location_id, table.event_group_id)
+]);
