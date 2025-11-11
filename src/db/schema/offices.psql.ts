87bfa42b484be07fab3055d038931dbf5ea7fd9b
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-12 00:29:28 +0800

    finished schema definition of offices talbe. only lacks relation definitions

--- src/db/schema/offices.psql.ts
+++ src/db/schema/offices.psql.ts
@@ -1,9 +1,13 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-import { timestamps } from './columns.helpers';
+import { serial, integer, pgTable, varchar, index } from "drizzle-orm/pg-core";
+import { departments } from "./departments.psql";
+import { schools } from "./schools.psql";
 
 export const offices = pgTable("offices", {
-    id: integer().primaryKey(),
+    id: serial().primaryKey(),
     name: varchar({ length: 255 }).notNull(),
-    department_id: integer(),
-    school_id: integer()
-});
+    department_id: integer().references(() => departments.id),
+    school_id: integer().references(() => schools.id)
+}, (table) => [
+    index("department_id_idx").on(table.department_id),
+    index("school_id_idx").on(table.school_id)
+]);
