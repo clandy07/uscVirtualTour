8eada4ecf7fc20faa2d47efca8ad8e18bb331b5e
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-19 23:53:02 +0800

    added buildings schema definition

---
+++ src/db/schema.ts
@@ -0,0 +1,16 @@
+import { smallint, integer, serial, pgTable, varchar, index } from "drizzle-orm/pg-core";
+import {timestamps} from './columns.helpers'
+
+export const buildings = pgTable("buildings", {
+    id: serial().primaryKey(),
+    name: varchar({ length: 255 }).notNull(),
+    campus_id: integer().references(() => campuses.id, {onDelete: 'cascade'}),
+    location_id: integer().references(() => locations.id, {onDelete: 'set null'}),
+    basement_count: smallint(),
+    floor_count: smallint(),
+    ...timestamps
+}, (table) => [
+    index("building_name_idx").on(table.name),
+    index("campus_idx").on(table.campus_id)
+]);
+
