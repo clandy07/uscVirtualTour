7ae064364e0698f019ade7684ffab36c6a04a468
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-29 22:59:38 +0800

    moved the file

--- src/app/api/orgs/[orgId]/events/[eventId]/[oldPlaceId]/route.ts
+++ src/app/api/orgs/[orgId]/events/[eventId]/[oldPlaceId]/route.ts
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { db } from '@/index';
-import { events } from '@/db/schema';
+import { events, event_location_relations, event_room_relations } from '@/db/schema';
 import { getUserRole } from '@/app/api/utils/auth';
 import { eq, and,} from 'drizzle-orm';
 import { checkAuth } from '@/app/api/utils/auth';
 import { getUserOrgs } from '@/app/api/utils/auth';
 import { getUserOrgPermissions } from '@/app/api/utils/auth';
+import {isNumber} from '@/app/utils'
 
 // GET /orgs/:orgId/events/:eventId - Get full details of a given event in a given org
 export async function GET(
@@ -121,6 +122,8 @@ export async function PATCH(
 
     try {
         const {orgId, eventId} = await params
+        const searchParams = request.nextUrl.searchParams
+        const inLocation:boolean = (searchParams.get('inLocation') === "true")
 
         const session = await checkAuth(request)
         if(!session){
@@ -147,12 +150,79 @@ export async function PATCH(
                     { status: 401 }
                 );       
             }
-            else{
-                // API logic for student who is a part of the given org and has permissions to patch them
-            }
         }
 
+        const body: {
+            name: string, 
+            description: string | null | undefined,
+            dateTimeStart: Date | string,
+            dateTimeEnd: Date | string | null | undefined,
+            visibility: "everyone" | "only_students" | "only_organization_members",
+            evetGroupId: number | null | undefined,
+            customMarker: string | null | undefined,
+            roomId: number | null | undefined,
+            locationId: number | null | undefined
+        } = await request.json();
+
         // admin logic goes here. admin cant update an event posted by students
+        const eventName = body.name;
+        const description = body.description;
+        const date_time_start = new Date(body.dateTimeStart);
+        const date_time_end = (body.dateTimeEnd) ? new Date(body.dateTimeEnd) : null;
+        const visibility = body.visibility;
+        const event_group_id = body.evetGroupId;
+        const custom_marker = body.customMarker;
+        const roomId = body.roomId;
+        const locationId = body.locationId;
+
+        if (!roomId && !locationId){
+            return NextResponse.json(
+                {
+                    error: "'roomId' and 'locationId' cannot be both null. Provide at least one."
+                },
+                { status: 400 }
+            );
+        }
+
+        const eventResult = await db.update(events).set(
+            {
+                name: eventName,
+                description: description,
+                date_time_start: date_time_start,
+                date_time_end: date_time_end,
+                custom_marker: custom_marker,
+                event_group_id: event_group_id,
+                visibility: visibility
+            }
+        )
+        .where(eq(events.id, parseInt(eventId)))
+        .returning({ updatedEventId: events.id });
+
+        const {updatedEventId} = eventResult[0]
+
+        let result;
+        if(inLocation && isNumber(locationId)){
+            result = await db.update(event_location_relations).set({
+                location_id: locationId
+            }).where(
+                and(
+                    eq(event_location_relations.event_id, updatedEventId),
+                    eq(event_location_relations.location_id, locationId)
+                )
+            );
+        }
+        else if(isNumber(roomId)){
+            result = await db.update(event_room_relations).set({
+                room_id: roomId
+            }).where(
+                and(
+                    eq(event_room_relations.event_id, updatedEventId),
+                    eq(event_room_relations.room_id, roomId)
+                )
+            );
+        }
+
+        return NextResponse.json({ data: eventResult });
 
     } catch (err){
         console.error(err);
