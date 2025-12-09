fce585c96a6f4db99c3fc5fd5804e2124034c7b7
Ian Sobrecaray <ian@sobrecaray.com>
2025-12-09 16:22:34 +0800

    Finished endpoint PATCH /geometries/:geometryId

--- src/app/api/geometries/[geometryId]/route.ts
+++ src/app/api/geometries/[geometryId]/route.ts
@@ -4,12 +4,57 @@ import { geometries } from '@/db/schema';
 import { requireAdmin } from '@/app/api/utils/auth';
 import { eq, and } from 'drizzle-orm';
 import { auth } from '@/lib/auth';
+import { isNumericString } from '@/app/utils';
+import { success } from 'better-auth';
 
 
-export async function GET(
+export async function PATCH(
   request: NextRequest,
   { params }: { params: Promise<{ geometryId: string; }> }
 ) {
+    try{
+        const authError = await requireAdmin(request)
+        if(authError) return authError
 
+        const {geometryId} = await params
+
+        const geometryIdNum = isNumericString(geometryId) ? parseInt(geometryId) : null
+        
+        if(geometryIdNum === null){
+            return NextResponse.json(
+                { error: 'Invalid geometry ID' },
+                { status: 400 }
+            )
+        }
+        
+        const body = await request.json()
+
+        const result = await db.update(geometries).set({
+            polygon: body.polygon
+        }).where(eq(geometries.id, geometryIdNum)).returning({
+            updatedGeometryId: geometries.id
+        })
+
+        return NextResponse.json({
+            success: true,
+            data: result
+        })
+    } catch(error){
+        console.error('Error updating the geometry', error);
+        return NextResponse.json(
+            { error: 'Failed to update the geometry' },
+            { status: 500 }
+        );
+    }
+
+}
+
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ geometryId: string; }> }
+) {
+    const authError = await requireAdmin(request)
+    if(authError) return authError
 
 }
